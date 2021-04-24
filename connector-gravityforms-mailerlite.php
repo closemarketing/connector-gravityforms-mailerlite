a2798536ed70a1152807be228934f61a00e17acc
David Perez <david@closemarketing.es>
2021-04-24 20:47:02 +0200

    updated version

--- connector-gravityforms-mailerlite.php
+++ connector-gravityforms-mailerlite.php
@@ -5,7 +5,7 @@
  * Description: Connects GravityForms with MailerLite.
  * Author: closemarketing
  * Author URI: https://www.closemarketing.es
- * Version: 1.3.3
+ * Version: 1.4
  *
  * Text Domain: connector-gravityforms-mailerlite
  *
@@ -20,7 +20,7 @@ defined( 'ABSPATH' ) || exit;
 // Loads translation.
 load_plugin_textdomain( 'connector-gravityforms-mailerlite', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 
-define( 'GF_CGFM_VERSION', '1.3.3' );
+define( 'GF_CGFM_VERSION', '1.4' );
 
 /**
  * Detect plugin WooCommerce Mailerlite
@@ -32,24 +32,6 @@ function cgm_is_plugin_active( $plugin ) {
 if ( ! cgm_is_plugin_active( 'woo-mailerlite/woo-mailerlite.php' ) ) {
 	// Plugin is activated.
 	require __DIR__ . '/vendor/autoload.php';
-}
-
-register_activation_hook( __FILE__, 'connector_gravityforms_activation_check' );
-/**
- * Checks for activated GravityForms before allowing plugin to activate
- *
- * @author David Perez
- * @connector_gravityforms_activation_check()
- * @since 1.3.2
- * @version 1.0
- */
-
-function connector_gravityforms_activation_check() {
-	// Restrict activation to only when the Genesis Framework is activated.
-	if ( ! cgm_is_plugin_active( 'gravityforms/gravityforms.php' ) && ! cgm_is_plugin_active( 'gravity-forms/gravityforms.php' ) ) {
-		deactivate_plugins( plugin_basename( __FILE__ ) );  // Deactivate ourself.
-		wp_die( sprintf( __( 'Whoa.. this plugin actually works, really, when you have installed the %1$sGravity Forms Plugin%2$s', 'connector-gravityforms-mailerlite' ), '<a href="https://www.closemarketing.es/go/gravityforms/" target="_new">', '</a>' ) );
-	}
 }
 
 // If Gravity Forms is loaded, bootstrap the Campaign Monitor Add-On.
@@ -79,7 +61,6 @@ class GF_CGFM_Bootstrap {
 		GFAddOn::register( 'GF_CGFM' );
 
 	}
-
 }
 
 /**
